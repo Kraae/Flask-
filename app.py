e6e03887b9d96f64bc0f798eb5f7a8afeadb9303
Kraae <117115729+Kraae@users.noreply.github.com>
2023-07-12 04:33:46 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+# Put your app in here.
+"""Flask Calculator"""
+"""
+Build a simple calculator with Flask, 
+which uses URL query parameters to get 
+the numbers to calculate with.
+
+Make a Flask app that responds to 4 different 
+routes. Each route does a math operation with 
+two numbers, a and b, which will be passed in
+as URL GET-style query parameters.
+
+/add
+Adds a and b and returns result as the body.
+/sub
+Same, subtracting b from a.
+/mult
+Same, multiplying a and b.
+/div
+Same, dividing a by b.
+
+For example, a URL like http://localhost:5000/add?a=10&b=20 
+should return a string response of exactly 30.
+
+Write the routes for this but don’t hardcode the math operation 
+in your route function directly. Instead, we’ve provided helper 
+functions for this in the file operations.py:
+"""
+
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route("/math/<oper>")
+def do_math(oper):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper] (a, b)
+    return str(result)
+
+operators = {
+    add: "add",
+    sub: "sub",
+    mult: "mult",
+    div: "div",
+}
+
+@app.route("/add")
+def do_add():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+    return str(result)
+
+@app.route("/sub")
+def do_sub():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+    return str(result)
+
+@app.route("/mult")
+def do_mult():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+    return str(result)
+
+@app.route("/div")
+def do_div():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+    return str(result)
+
